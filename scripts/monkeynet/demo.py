709e066ee9e40b96b102a554be26cf9e995a95e8
jinchengbin <jinchengbin@huya.com>
2021-05-26 10:22:38 +0800

    add moneky demo file

---
+++ scripts/monkeynet/demo.py
@@ -0,0 +1,20 @@
+import yaml
+import matplotlib
+import imageio
+import torch
+import numpy as np
+from argparse import ArgumentParser
+
+# from transfer import transfer_one
+# from logger import Logger
+# from modules.generator import MotionTransferGenerator
+# from modules.keypoint_detector import KPDetector
+# from sync_batchnorm import DataParallelWithCallback
+# from frames_dataset import read_video
+# from augmentation import VideoToTensor
+
+
+if __name__ == "__main__":
+    print("Hello world!")
+
+
