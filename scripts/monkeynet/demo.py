8605852e19c08310bc61a8900b93777f37298a43
jinchengbin <jinchengbin@huya.com>
2021-05-26 16:18:46 +0800

    add argparser

--- scripts/monkeynet/demo.py
+++ scripts/monkeynet/demo.py
@@ -15,6 +15,19 @@ from argparse import ArgumentParser
 
 
 if __name__ == "__main__":
-    print("Hello world!")
+    parser = ArgumentParser()
+    parser.add_argument("--config", required=True, help="path to config")
+    parser.add_argument("--checkpoint", required=True, help="path to checkpoint")
+    parser.add_argument("--source_image", default="sup-mat/source.png", help="path to source image")
+    parser.add_argument("--driving_video", default="sup-mat/driving.png", help="path to driving video")
+    parser.add_argument("--out_file", default="demo.gif", help="path to out file")
+    parser.add_argument("--cpu", dest="cpu", action="store_true", help="use cpu")
+    opt = parser.parse_args()
 
+    with open(opt.config) as f:
+        config = yaml.load(f, Loader=yaml.FullLoader)
+        blocks_discrimiantor = config['model_params']['discriminator_params']['num_blocks']
+        assert len(config['train_params']['loss_weights']['reconstruction']) == blocks_discrimiantor + 1
+
+    print(f"Hello world!")
 
