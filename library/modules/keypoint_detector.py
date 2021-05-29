088c79f34047af4a38c116c5f9737c2270afb489
jinchengbin <jinchengbin@huya.com>
2021-05-29 16:42:44 +0800

    add gaussian2kp & smallest_singular

--- library/modules/keypoint_detector.py
+++ library/modules/keypoint_detector.py
@@ -2,7 +2,7 @@ import torch
 import torch.nn.functional as F
 from torch import nn
 
-from library.modules.util import Hourglass, make_coordinate_grid, matrix_inverse
+from library.modules.util import Hourglass, make_coordinate_grid, matrix_inverse, smallest_singular
 
 
 class KPDetector(nn.Module):
@@ -71,4 +71,36 @@ def kp2gaussian(kp, spatial_size, kp_variance='matrix'):
 
 
 def gaussian2kp(heatmap, kp_variance='matrix', clip_variance=None):
-    return heatmap
+    """
+    Extract the mean and the variance from a heatmp
+    """
+    shape = heatmap.shape                   # [N, kp, 3, H/2, W/2]
+    # adding small eps to avoid 'nan' in variance
+    heatmap = heatmap.unsqueeze(-1) + 1e-7  # [N, kp, 3, H/2, W/2, 1]
+    grid = make_coordinate_grid(shape[3:], heatmap.type()).unsqueeze_(0).unsqueeze_(0).unsqueeze_(0)  # [1, 1, 1, H/2, W/2, 2]
+
+    mean = (heatmap * grid).sum(dim=(3, 4))  # [N, kp, 3, 2]
+    kp = {'mean': mean.permute(0, 2, 1, 3)}  # [N, 3, kp, 2]
+
+    if kp_variance == 'matrix':
+        mean_sub = grid - mean.unsqueeze(-2).unsqueeze(-2)  # [N, kp, 3, H/2, W/2, 2]
+        var = torch.matmul(mean_sub.unsqueeze(-1), mean_sub.unsqueeze(-2))  # [N, kp, 3, H/2, W/2, 2, 2]
+        var = var * heatmap.unsqueeze(-1)                   # [N, kp, 3, H/2, W/2, 2, 2]
+        var = var.sum(dim=(3, 4))                           # [N, kp, 3, 2, 2]
+        var = var.permute(0, 2, 1, 3, 4)                    # [N, 3, kp, 2, 2]
+        if clip_variance:
+            min_norm = torch.tensor(clip_variance).type(var.type())
+            sg = smallest_singular(var).unsqueeze(-1)
+            var = torch.max(min_norm, sg) * var / sg
+        kp['var'] = var
+    elif kp_variance == 'single':
+        mean_sub = grid - mean.unsqueeze(-2).unsqueeze(-2)  # [N, kp, 3, H/2, W/2, 2]
+        var = mean_sub ** 2                                 # [N, kp, 3, H/2, W/2, 2]
+        var = var * heatmap                                 # [N, kp, 3, H/2, W/2, 2]
+        var = var.sum(dim=(3, 4))                           # [N, kp, 3, 2]
+        var = var.mean(dim=-1, keepdim=True)                # [N, kp, 3, 1]
+        var = var.unsqueeze(-1)                             # [N, kp, 3, 1, 1]
+        var = var.permute(0, 2, 1, 3, 4)                    # [N, 3, kp, 1, 1]
+        kp['var'] = var
+
+    return kp
--- library/modules/util.py
+++ library/modules/util.py
@@ -163,3 +163,17 @@ def matrix_inverse(batch_of_matrix, eps=0):
         eye = b_mat.new_ones(b_mat.size(-1)).diag().expand_as(b_mat)
         b_inv, _ = torch.solve(eye, b_mat)
         return b_inv
+
+
+def smallest_singular(batch_of_matrix):
+    a = (batch_of_matrix[..., 0, 0]).unsqueeze(-1)
+    b = (batch_of_matrix[..., 0, 1]).unsqueeze(-1)
+    c = (batch_of_matrix[..., 1, 0]).unsqueeze(-1)
+    d = (batch_of_matrix[..., 1, 1]).unsqueeze(-1)
+
+    s1 = a ** 2 + b ** 2 + c ** 2 + d ** 2
+    s2 = (a ** 2 + b ** 2 - c ** 2 - d ** 2) ** 2
+    s2 = torch.sqrt(s2 + 4 * (a * c + b * d) ** 2)
+    norm = torch.sqrt((s1 - s2) / 2)
+
+    return norm
