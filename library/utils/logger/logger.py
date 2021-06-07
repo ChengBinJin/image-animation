10e1c917e43dc768948db165fdd7bd1cc7d1dda4
jinchengbin <jinchengbin@huya.com>
2021-06-07 22:39:56 +0800

    add draw_video_with_kp

--- library/utils/logger/logger.py
+++ library/utils/logger/logger.py
@@ -3,6 +3,7 @@ import imageio
 import torch
 import numpy as np
 import matplotlib.pyplot as plt
+from skimage.draw import circle
 
 
 class Logger:
@@ -94,3 +95,15 @@ class Visualizer:
         self.kp_size = kp_size
         self.draw_border = draw_border
         self.colormap = plt.get_cmap(colormap)
+
+    def draw_video_with_kp(self, video, kp_array):
+        video_array = np.copy(video)
+        spatial_size = np.array(video_array.shape[2:0:-1])[np.newaxis, np.newaxis]
+        kp_array = spatial_size * (kp_array + 1) / 2
+        num_kp = kp_array.shape[1]
+
+        for i in range(len(video_array)):
+            for kp_ind, kp in enumerate(kp_array[i]):
+                rr, cc = circle(kp[1], kp[0], self.kp_size, shape=video_array.shape[1:3])
+                video_array[i][rr, cc] = np.array(self.colormap(kp_ind / num_kp))[:3]
+        return video_array
