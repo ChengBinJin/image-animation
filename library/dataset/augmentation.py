323f07662da2818f560bd855dc4c0da732a59c44
jinchengbin <jinchengbin@huya.com>
2021-06-26 20:13:51 +0800

    add random-flip and select-random-frames

--- library/dataset/augmentation.py
+++ library/dataset/augmentation.py
@@ -1,3 +1,5 @@
+import random
+
 import numpy as np
 
 
@@ -8,6 +10,44 @@ class VideoToTensor(object):
         driving = np.array(driving, dtype='float32')
         out = {'video': driving.transpose((3, 0, 1, 2))}  # (C, N, H, W)
         return out
+
+
+class SelectRandomFrames(object):
+    def __init__(self, consequent=False, number_of_frames=2):
+        self.consequent = consequent
+        self.num_frames_to_select = number_of_frames
+
+    def __call__(self, clip):
+        """
+        Args:
+            img (PIL.Image or numpy.ndarray): List of images for selection in format (h, w, c) in numpy.ndarray
+        Returns:
+            PIL.Image or numpy.ndarray: List of number_of_frames images
+        """
+        frame_count = len(clip)
+
+        if self.consequent:
+            first_frame = np.random.choice(max(1, frame_count - self.num_frames_to_select + 1), size=1)[0]
+            selected = clip[first_frame:first_frame + self.num_frames_to_select]
+        else:
+            selected_index = np.sort(np.random.choice(range(frame_count), replace=True, size=self.num_frames_to_select))
+            selected = clip[selected_index]
+
+        return selected
+
+
+class RandomFlip(object):
+    def __init__(self, time_flip=False, horizontal_flip=False):
+        self.time_flip = time_flip
+        self.horizontal_flip = horizontal_flip
+
+    def __call__(self, clip):
+        if random.random() > 0.5 and self.time_flip:
+            clip = clip[::-1]
+        if random.random() > 0.5 and self.horizontal_flip:
+            clip = [np.fliplr(img) for img in clip]
+
+        return clip
 
 
 class AllAumgnetationTransform:
