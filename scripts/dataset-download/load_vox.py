7ca3a2cab696e985df8bc88c55b4cdd7766ad792
jinchengbin <jinchengbin@huya.com>
2021-07-08 20:01:37 +0800

    add load_vox file

--- scripts/dataset-download/load_vox.py
+++ scripts/dataset-download/load_vox.py
@@ -1,3 +1,22 @@
+import sys
+import os
+from argparse import ArgumentParser
+
+project_dir = os.path.dirname(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
+sys.path.append(project_dir)
+
+parser = ArgumentParser()
+parser.add_argument("--annotations_dir", default=r"scripts\dataset-download\VoxCeleb\vox1_test_txt")
+parser.add_argument("--save_dir", default=r"D:\Data\image_animation\VoxCeleb\vox1_test")
+args = parser.parse_args()
+
+
+def main():
+    print("Hello world!")
+
+
 if __name__ == "__main__":
-    parser = ArgumentParser()
-    parser.add_argument("--annotations_folder", default=)
+    if not os.path.exists(args.save_dir):
+        os.makedirs(args.save_dir)
+
+    main()
